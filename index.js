69a4e2c07bf7632aab040874b391194a661e967d
tmatschek <51510905+tmatschek@users.noreply.github.com>
2023-02-26 20:53:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ express()
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.setViewport({ width: 600, height: 800 });
-    await page.goto(process.env.SCREENSHOT_URL || 'https://tempestwx.com/station/82937/');
+    await page.goto(process.env.SCREENSHOT_URL || 'https://tempestwx.com/station/82937');
     await page.screenshot({
       path: '/tmp/screenshot.png',
     });
